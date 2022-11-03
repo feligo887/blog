9245b098846662152baa2927555335c4d54157e1
feligo.fish@qq.com <feligo.54285933>
2022-11-03 21:56:00 +0800

    docs...

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,15 @@
 set -e
 
 # 生成静态文件
-npm run build
+# npm run build
 
 # 进入生成的文件夹
 cd blog/.vuepress/dist
 
-git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:feligo887/feligo887.github.io.git main
+git push origin main
 
-cd ../ && rm -rf dist
+cd -
